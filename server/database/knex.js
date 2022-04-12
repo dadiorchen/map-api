791114706942797e79ff4f0523cc8c9dba14371d
Dadiorchen <dadiorchen@outlook.com>
2022-04-12 15:16:59 +0800

    Initial commit

---
+++ server/database/knex.js
@@ -0,0 +1,22 @@
+const expect = require('expect-runtime');
+const log = require('loglevel');
+const knex = require('knex');
+
+const connection = process.env.DATABASE_URL;
+expect(connection).to.match(/^postgresql:\//);
+
+const knexConfig = {
+  client: 'pg',
+  debug: process.env.NODE_LOG_LEVEL === 'debug',
+  connection,
+  pool: { min: 0, max: 100 },
+};
+
+log.debug(process.env.DATABASE_SCHEMA);
+if (process.env.DATABASE_SCHEMA) {
+  log.info('setting a schema');
+  knexConfig.searchPath = [process.env.DATABASE_SCHEMA];
+}
+log.debug(knexConfig.searchPath);
+
+module.exports = knex(knexConfig);
