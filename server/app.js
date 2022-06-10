2e1cc0013f474dac713e424b297e12247a9605a5
deanchen <antlet2004@gmail.com>
2022-06-10 10:37:21 +0800

    feat: can get/post theme

--- server/app.js
+++ server/app.js
@@ -8,8 +8,6 @@ const HttpError = require('./utils/HttpError');
 // const { errorHandler } = require('./utils/utils');
 const { handlerWrapper } = require('./utils/utils');
 
-const knex = require('knex');
-
 const memoryStore = new session.MemoryStore();
 
 
@@ -24,10 +22,10 @@ if (process.env.NODE_ENV === 'development') {
   app.use(cors());
 }
 
-app.use(keycloak.middleware({
-  logout: '/logout',
-  admin: '/'
-}));
+// app.use(keycloak.middleware({
+//   logout: '/logout',
+//   admin: '/'
+// }));
 
 /*
  * Check request
@@ -67,20 +65,51 @@ app.get('/public', async (req, res) => {
 
 app.get('/settings', keycloak.enforcer('web-map-global-setting:view'), (req, res) => res.status(200).json({ ok: true }));
 
-app.post('/organizations/:organization_id/theme', async (req, res) => {
+const knex = require('knex')({
+  client: 'postgresql',
+  connection: process.env.DB_URL,
+  searchPath: 'webmap',
+  debug: true,
+});
+
+app.get('/organizations/:organization_id/theme', async (req, res) => {
   const organization_id = req.params.organization_id;
   const theme = req.body.theme;
   // update database
   try {
-    // init knex with schema 'webmap' and connection to postgres database
-    const knex = require('knex')({
-      client: 'postgresql',
-      connection: process.env.DB_URL,
-      searchPath: 'public',
-    });
+    // select theme from database
     const result = await knex('map_config')
-      .where('organization_id', organization_id)
-      .update({ theme });
+      .select('theme')
+      .where({ organization_id });
+    if (result.length === 0) {
+      res.status(404).json({
+        message: "not found",
+      });
+    }
+    res.status(200).json(result[0]);
+  } catch (e) {
+    console.log("error:", e);
+    res.status(500).json({ error: 500, message: "get error when select" });
+  }
+})
+
+app.post('/organizations/:organization_id/theme', async (req, res) => {
+  const organization_id = req.params.organization_id;
+  console.warn("organization_id:", organization_id);
+  const theme = req.body.theme;
+  // update database
+  try {
+    const sql = `
+      INSERT INTO map_config 
+      (organization_id, theme) 
+      VALUES
+      (${organization_id}, ?)
+      ON CONFLICT (organization_id) DO UPDATE 
+        SET theme = ?
+    `
+    // run knex with sql and params
+    const result = await knex.raw(sql, [theme, theme]);
+
     res.status(200).json({ ok: true });
   } catch (e) {
     console.log("error:", e);
