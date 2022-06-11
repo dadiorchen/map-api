65cbfb0b9f1bc76f1c5f5abebe6a22df5dfdd3a0
deanchen <antlet2004@gmail.com>
2022-06-11 15:02:12 +0800

    feat: be able to edit theme

--- server/app.js
+++ server/app.js
@@ -95,13 +95,15 @@ app.get('/organizations/:organization_id/theme',
     }
   })
 
-app.post('/organizations/:organization_id/theme', async (req, res) => {
-  const organization_id = req.params.organization_id;
-  console.warn("organization_id:", organization_id);
-  const theme = req.body.theme;
-  // update database
-  try {
-    const sql = `
+app.post('/organizations/:organization_id/theme',
+  keycloak.enforcer('web-map-theme:edit'),
+  async (req, res) => {
+    const organization_id = req.params.organization_id;
+    console.warn("organization_id:", organization_id);
+    const theme = req.body.theme;
+    // update database
+    try {
+      const sql = `
       INSERT INTO map_config 
       (organization_id, theme) 
       VALUES
@@ -109,15 +111,15 @@ app.post('/organizations/:organization_id/theme', async (req, res) => {
       ON CONFLICT (organization_id) DO UPDATE 
         SET theme = ?
     `
-    // run knex with sql and params
-    const result = await knex.raw(sql, [theme, theme]);
+      // run knex with sql and params
+      const result = await knex.raw(sql, [theme, theme]);
 
-    res.status(200).json({ ok: true });
-  } catch (e) {
-    console.log("error:", e);
-    res.status(500).json({ error: 500, message: "get error when update" });
-  }
-})
+      res.status(200).json({ ok: true });
+    } catch (e) {
+      console.log("error:", e);
+      res.status(500).json({ error: 500, message: "get error when update" });
+    }
+  })
 
 // Global error handler
 // app.use(errorHandler);
